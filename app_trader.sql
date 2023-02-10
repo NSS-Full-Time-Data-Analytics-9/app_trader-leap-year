72074cd034a06b3704dcd569a25b84825f2d9585
Brian Wilhelm <bswilhelm@gmail.com>
2023-02-10 15:57:24 -0600

    friday push

--- app_trader.sql
+++ app_trader.sql
@@ -1,131 +1,38 @@
-SELECT *
-FROM app_store_apps;
-
-SELECT *
-FROM play_store_apps;
-
---DURATION--
-(SELECT name, 
-		rating,
-		(rating * 2) + 1 AS year,
- 		primary_genre AS genres
-FROM app_store_apps)
-UNION ALL
-(SELECT name, 
-		rating,
-		(rating * 2) + 1 AS year,
- 		genres
-FROM play_store_apps);
-
---BROKER COSTS--
-SELECT name, price,
-	  CASE WHEN price > 2.50 THEN price*10000
-	  ELSE 25000 END AS broker_cost
-FROM app_store_apps;
-
-SELECT name, price,
-	  CASE WHEN price::money::numeric > 2.50 THEN price::money::numeric*10000
-	  ELSE 25000 END AS broker_cost
-FROM play_store_apps;
-
-
---BOTH PLATFORMS--
-SELECT name
-FROM app_store_apps
-WHERE name IN
-		(SELECT name
-		 FROM play_store_apps);
-
---PLAY_STORE_APP sorted with DISTINCT name--
-SELECT DISTINCT name,
-		MAX(rating) AS rating,
-		MAX(review_count) AS review_count,
-		MAX(install_count) AS install_count,
-		MAX(price::money) AS price,
-		MAX(content_rating) AS content_rating,
-		MAX(genres) As genres
-FROM play_store_apps
-GROUP BY name;
-
---APP_STORE_APPS sorted with DISTINCT name--
-SELECT DISTINCT name,
-		MAX(rating) AS rating,
-		MAX(review_count) AS review_count,
-		MAX(price::money) AS price,
-		MAX(content_rating) AS content_rating,
-		MAX(primary_genre) AS genres
-FROM app_store_apps
-GROUP BY name;
-
-
 --ALL APPs that appear in both with ALL DATA JOINED--
 WITH app_sorted AS (SELECT DISTINCT name,
 								MAX(rating) AS app_rating,
+								(rating * 2) + 1 AS app_duration_years,
 								MAX(review_count) AS app_review_count,
-								MAX(price::money) AS app_price,
+								MAX(price) AS app_price,
 								MAX(content_rating) AS app_content_rating,
-								MAX(primary_genre) AS app_genres
+								MAX(primary_genre) AS app_genres,
+								CASE WHEN price > 2.50 THEN ROUND(price *10000, 0)
+	  								ELSE 25000 END AS app_broker_cost
 					FROM app_store_apps
-					GROUP BY name),
+					GROUP BY name, price, rating),
 	play_sorted AS (SELECT DISTINCT name,
 								MAX(rating) AS play_rating,
+								MAX(rating) * 2 + 1 AS play_duration_years,
 								MAX(review_count) AS play_review_count,
 								MAX(install_count) AS play_install_count,
-								MAX(price::money) AS play_price,
+								MAX(price::money::numeric) AS play_price,
 								MAX(content_rating) AS play_content_rating,
-								MAX(genres) As play_genres
+								MAX(genres) As play_genres,
+								CASE WHEN price::money::numeric > 2.50 THEN ROUND(price::money::numeric*10000, 0)
+	  								ELSE 25000 END AS play_broker_cost,
+								MAX(category) AS category
 					FROM play_store_apps
-					GROUP BY name)
+					GROUP BY name, price)
 SELECT *
 FROM play_sorted
 		INNER JOIN app_sorted USING(name);
-		
-
---RATINGS grouped by GENRE--
-SELECT primary_genre,
-		COUNT(rating) AS num_ratings,
-		AVG(rating) AS avg_rating,
-		ROUND(AVG(review_count::numeric),0) AS avg_reviews,
-		ROUND(AVG(price),2) AS avg_price
-FROM app_store_apps
-GROUP BY primary_genre
-ORDER BY avg_rating DESC;
-
-SELECT genres,
-		COUNT(rating) AS num_ratings,
-		AVG(rating) AS avg_rating,
-		ROUND(AVG(review_count),0) AS avg_reviews,
-		ROUND(AVG(price::money::numeric),2) AS avg_price
-FROM play_store_apps
-GROUP BY genres
-ORDER BY avg_rating DESC NULLS LAST;
-
---Cost / Duration / App_income
-SELECT name,	
-			CASE WHEN price > 2.50 THEN ROUND(price*10000, 0)
-	  		ELSE 25000 END AS broker_cost,
-		(rating * 2) + 1 AS app_duration_years,
-		((rating * 2) + 1) * (1500*12) AS app_income,
-		review_count
-FROM app_store_apps
-GROUP BY name, price, rating, review_count
-ORDER BY name;
-
-SELECT name,	
-			CASE WHEN price::money::numeric > 2.50 THEN ROUND(price::money::numeric*10000, 0)
-	  		ELSE 25000 END AS broker_cost,
-		(rating * 2) + 1 AS app_duration_years,
-		ROUND(((rating * 2) + 1) * (1500*12), 0) AS app_income,
-		review_count
-FROM play_store_apps
-GROUP BY name, price, rating, review_count
-ORDER BY name;
 
 
+--PRICING--
 WITH app_sorted AS (SELECT DISTINCT name,
 								MAX(rating) AS app_rating,
 								MAX(review_count) AS app_review_count,
-								MAX(price::money) AS app_price,
+								MAX(price) AS app_price,
 								MAX(content_rating) AS app_content_rating,
 								MAX(primary_genre) AS app_genres
 					FROM app_store_apps
@@ -134,20 +41,112 @@ WITH app_sorted AS (SELECT DISTINCT name,
 								MAX(rating) AS play_rating,
 								MAX(review_count) AS play_review_count,
 								MAX(install_count) AS play_install_count,
-								MAX(price::money) AS play_price,
+								MAX(price::money::numeric) AS play_price,
 								MAX(content_rating) AS play_content_rating,
 								MAX(genres) As play_genres
 					FROM play_store_apps
 					GROUP BY name)
-SELECT name,
-		CASE WHEN play_price::numeric > 2.50 THEN ROUND(play_price::numeric*10000, 0)
-	  		ELSE 25000 END AS broker_cost_play,
-		CASE WHEN app_price::numeric > 2.50 THEN ROUND(app_price::numeric*10000, 0)
-	  		ELSE 25000 END AS broker_cost_app
+SELECT name, 
+		app_price,
+		play_price
 FROM play_sorted
-		INNER JOIN app_sorted USING(name);
+		INNER JOIN app_sorted USING(name)
+ORDER BY app_price DESC;
+--80% of APPs are FREE 
+
+--GENRE--
+WITH app_sorted AS (SELECT DISTINCT name,
+								MAX(rating) AS app_rating,
+								MAX(review_count) AS app_review_count,
+								MAX(price) AS app_price,
+								MAX(content_rating) AS app_content_rating,
+								MAX(primary_genre) AS app_genres
+					FROM app_store_apps
+					GROUP BY name),
+	play_sorted AS (SELECT DISTINCT name,
+								MAX(rating) AS play_rating,
+								MAX(review_count) AS play_review_count,
+								MAX(install_count) AS play_install_count,
+								MAX(price::money::numeric) AS play_price,
+								MAX(content_rating) AS play_content_rating,
+								MAX(genres) As play_genres
+					FROM play_store_apps
+					GROUP BY name)
+SELECT app_genres,
+		COUNT(*) AS num_apps,
+		ROUND(AVG(app_rating),2) As avg_rating,
+		ROUND(AVG(app_price),2) AS avg_price
+FROM play_sorted
+		INNER JOIN app_sorted USING(name)
+GROUP BY app_genres
+ORDER BY num_apps DESC;
+--GAMES most popular (174/328) - AVG(price) 
 
 
-		 
+--CONTENT RATING--
+WITH app_sorted AS (SELECT DISTINCT name,
+								MAX(rating) AS app_rating,
+								MAX(review_count) AS app_review_count,
+								MAX(price) AS app_price,
+								MAX(content_rating) AS app_content_rating,
+								MAX(primary_genre) AS app_genres
+					FROM app_store_apps
+					GROUP BY name),
+	play_sorted AS (SELECT DISTINCT name,
+								MAX(rating) AS play_rating,
+								MAX(review_count) AS play_review_count,
+								MAX(install_count) AS play_install_count,
+								MAX(price::money::numeric) AS play_price,
+								MAX(content_rating) AS play_content_rating,
+								MAX(genres) As play_genres
+					FROM play_store_apps
+					GROUP BY name)
+SELECT play_content_rating,
+		COUNT(*),
+		ROUND(AVG(play_rating),2) AS avg_rating,
+		ROUND(AVG(play_price), 2) AS avg_price
+FROM play_sorted
+		INNER JOIN app_sorted USING(name)
+GROUP BY play_content_rating
+ORDER BY avg_rating DESC;
+--'Everyone 10+' had highest AVG(rating) with losest AVG(price)-- 
+
+
+--PROFITABILITY--
+WITH app_sorted AS (SELECT DISTINCT name,
+								MAX(rating) AS app_rating,
+								(rating * 2) + 1 AS app_duration_years,
+								MAX(review_count) AS app_review_count,
+								MAX(price) AS app_price,
+								MAX(content_rating) AS app_content_rating,
+								MAX(primary_genre) AS app_genres,
+								CASE WHEN price > 2.50 THEN ROUND(price *10000, 0)
+	  								ELSE 25000 END AS app_broker_cost
+					FROM app_store_apps
+					GROUP BY name, price, rating),
+	play_sorted AS (SELECT DISTINCT name,
+								MAX(rating) AS play_rating,
+								MAX(rating) * 2 + 1 AS play_duration_years,
+								MAX(review_count) AS play_review_count,
+								MAX(install_count) AS play_install_count,
+								MAX(price::money::numeric) AS play_price,
+								MAX(content_rating) AS play_content_rating,
+								MAX(genres) As play_genres,
+								CASE WHEN price::money::numeric > 2.50 THEN ROUND(price::money::numeric*10000, 0)
+	  								ELSE 25000 END AS play_broker_cost,
+								MAX(category) AS category
+
+					FROM play_store_apps
+					GROUP BY name, price)
+SELECT name,
+		(play_duration_years * (4500*12)) - play_broker_cost AS app_income,
+		app_genres,
+		play_content_rating,
+		play_install_count,
+		play_price,
+		play_duration_years
+FROM play_sorted
+		INNER JOIN app_sorted USING(name)
+ORDER BY app_income DESC;
 
 
